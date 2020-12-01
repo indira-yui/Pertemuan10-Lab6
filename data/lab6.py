d1c74fe8f22a415c736259d4682f36e362b48a9d
indira-yui <indiradira63@gmail.com>
2020-12-01 11:38:27 +0700

    update folder

--- data/lab6.py
+++ data/lab6.py
@@ -1,26 +1,3 @@
-_________________________________________________________________________________
-
-# LAB 6
-
-![Latihan 1](gambar/gambar1.jpg) <br>
-
-Pada tugas LAB 6, saya akan membuat sebuah program menambahkan data ke sebuah list dengan sistem library root yang nantinya akan seperti ini. <br>
-
-
-## Langkah - Langkah
-* Membuat file library root-nya dengan nama folder yaitu (data) <br>
-
-![New Folder](gambar/gambar2.jpg) <br>
-
-![New Folder](gambar/gambar3.jpg) <br>
-
-* Lalu, membuat file python dengan nama (lab6.py) atau bisa juga memberikan nama lainnya <br>
-
-![File Python](gambar/gambar4.jpg) <br>
-
-Setelah itu, coding file tersebut dengan syntax seperti di bawah ini : <br>
-
-```python
 data = []
 data = {}
 
@@ -73,48 +50,3 @@ def hapus():
             del data[nama]
         else:
             print("Data Nilai Tidak Ada".format(nama))
-
-```
-Jika sudah memasukan semua syntax diatas jangan di-run terlebih dahulu nanti file tersebut akan rusak/tidak dapat dijalankan dan hasilnya akan error. <br>
-
-* Selanjutnya, membuat file python tetapi di luar folder (data) untuk menjalankan file sebelumnya di-coding. File yang telah dibuat berikan nama (main.py) karena akan menjadi file yang akan menjalankan file library root yang telah dibuat sebelumnya <br>
-
-![File Python](gambar/gambar5.jpg) <br>
-
-* Lanjutkan dengan coding file tersebut dengan syntax seperti di bawah ini : <br>
-
-```python
-from data import lab6
-
-print("PROGRAM MENAMPILKAN DAFATR NILAI MAHASISWA")
-while True:
-    print("")
-    c =input("(L)lihat, (T)ambah, (U)bah, (H)apus, (K)eluar : ")
-    if c.lower() == 't':
-        lab6.tambah()
-
-    elif c.lower() == 'u':
-        lab6.ubah()
-
-    elif c.lower() == 'l':
-        lab6.lihat()
-
-    elif c.lower() == 'h':
-        lab6.hapus()
-
-    elif c.lower() == 'k':
-        print("Keluar")
-        break
-
-```
-
-Terlihat di awal terdapat syntax seperti ini: <br>
-```
-from data import lab6
-```
-
-Singkat penjelasan, fungsinya untuk menjalankan file library rootnya yaitu dari folder bernama (data) untuk membuka file (lab6.py). Jika sudah save dan run. Outputnya akan terlihat seperti di bawah ini. <br>
-
-![Output](gambar/gambar6.jpg) <br>
-
-___________________________________________________________________________________________________
